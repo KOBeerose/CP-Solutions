03a76dff6041c5f31a774ac836fa50fb91b36341
KOBeerose <tahaelghabi@gmail.com>
2022-08-22 09:55:07 +0100

    day2

---
+++ LeetCode/SQL Study Plan/day2/calculate-special-bonus.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+
+# SELECT employee_id, 
+# CASE WHEN employee_id%2=1 AND name NOT LIKE "M%" 
+#     THEN salary
+#     ELSE 0
+# END AS 'bonus'
+# FROM Employees
+# ORDER BY employee_id
+
+SELECT employee_id, 
+    (IF (name NOT LIKE "M%" AND employee_id%2=1, salary, 0 )) AS bonus 
+FROM Employees
+ORDER BY employee_id
---
+++ LeetCode/SQL Study Plan/day2/notes.md
@@ -0,0 +1,4 @@
+### 1873. Calculate Special Bonus
+Start with => "M%"
+**You can use If in place of a column**
+IF(condition, true_value, false_value) as Column name
