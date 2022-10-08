501e886b0e27fdf19ac938032107b2b7ed48a7e3
KOBeerose <tahaelghabi@gmail.com>
2022-10-08 22:50:56 +0100

    validate subsequences

--- LeetCode/DSA/remove-duplicates-sorted-array.py
+++ LeetCode/DSA/remove-duplicates-sorted-array.py
@@ -6,7 +6,6 @@ for i in range(len(array)-1):
     if array[i] != array[i+1]:      
         array[index] = array[i] 
         index+=1  
-
 array[index] = array[len(array)-1]
 print(array[:index+1])
 
